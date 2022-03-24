77515ea833f2439abd2b8c4d96bd51e608c4613f
cchen <cchen@artisreit.com>
2022-03-24 16:58:35 -0500

    playlist

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ server.listen(8080, function () {
   console.log(`Listening on http://localhost:8080`);
     // /Applications/Firefox.app/Contents/MacOS/firefox -kiosk https://www.google.com
     const { exec } = require('child_process');
-    exec('"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe" --kiosk http://localhost:8080 --edge-kiosk-type=fullscreen', (error, stdout, stderr) => { 
+    //exec('"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe" --kiosk http://localhost:8080 --edge-kiosk-type=fullscreen', (error, stdout, stderr) => { 
+    exec('/Applications/Firefox.app/Contents/MacOS/firefox -kiosk http://localhost:8080', (error, stdout, stderr) => { 
     if (error) {
         console.error(`exec error: ${error}`);
         return;
